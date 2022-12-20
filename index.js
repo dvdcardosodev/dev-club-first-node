ca11008757cb66c65e60aa2c407678ed78e65de5
dvdcardosodev <dfvcardoso@outlook.com>
2022-12-19 21:07:40 -0300

    First project

---
+++ index.js
@@ -0,0 +1,66 @@
+const { response } = require('express')
+const express = require('express')
+const uuid = require('uuid')
+
+const port = 3000
+const server = express()
+server.use(express.json())
+
+const users=[]
+
+
+server.get('/users', (request, response) => {
+
+return response.json(users)
+
+})
+
+server.post('/users', (request, response) => {
+
+    const {name, age} = request.body
+
+    const user ={id:uuid.v4(), name, age}
+
+    users.push(user)
+
+    return response.status(201).json(user)
+    
+})
+
+server.put('/users/:id', (request, response) => {
+
+    const{id}= request.params
+    const{name, age}= request.body
+
+    const updatedUser= {id, name, age}
+    
+    const index= users.findIndex(user => user.id===id)
+
+    if(index < 0){
+        return response.status(404).json({message:"User not found"})
+
+    }
+    
+    users[index] = updatedUser
+
+    return response.json(updatedUser)
+    
+    
+})
+
+
+server.delete('/users:id', (request, response) => {
+    const {id} = request.params
+
+    const index= users.findIndex(user => user.id===id)
+
+    if(index < 0){
+        return response.status(404).json({message:"User not found"})
+
+    }
+    
+    users.splice(index,1)
+
+    return response.status(204).json()
+    
+})
